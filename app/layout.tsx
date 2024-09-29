30b3a4c957b7760fde300a5b713f794da121163d
mamta-vyas <112095415+mamta-vyas@users.noreply.github.com>
2024-09-29 15:32:42 +0530

    initialization of project

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+// import localFont from "next/font/local";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+import Nav from "./components/Home/Nav";
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+const font = Plus_Jakarta_Sans({ 
+  weight: ['200' , '300' , '400' , '500' ,'600' , '700' , '800'],
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: "HireHub | Find Your Dream Job",
+  description: "Find Your Dream Job",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Nav/>
+        {children}</body>
+    </html>
+  );
+}
