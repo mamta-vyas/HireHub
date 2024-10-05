74e1318e4be3f7981043c4f915f354345888694b
mamta-vyas <112095415+mamta-vyas@users.noreply.github.com>
2024-10-05 12:33:04 +0530

    some changes

--- app/components/Home/Nav.tsx
+++ app/components/Home/Nav.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import LogoImage from "@/public/images/logo.jpeg" 
+import LogoImage from "@/public/images/logo.jpg" 
 import Link from 'next/link'
 import Image from 'next/image'
 import { getServerSession } from 'next-auth'
